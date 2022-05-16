9249404484b615461404b5c5f2e5693de0de841a
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-05-16 19:29:47 -0300

    Create lista2.c

---
+++ lista2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// Exercicio 1
+// a)
+void main()
+{
+    int a = 40; // cria uma variável do tipo inteiro, chamada a, e inicializa com valor 40;
+    int *p; // cria uma variável do tipo ponteiro para inteiro, chamada p, e o conteúdo inicial é lixo;
+    p = &a; // faz p receber o endereço de a. Dizemos que p aponta para a
+    printf("%p \n", &a);
+    printf("%p", p);
+    return 0;
+}
+
+// b)
+
+void main()
+{
+    int a = 40; // cria uma variável do tipo inteiro, chamada a, e inicializa com valor 40;
+    int *p; // cria uma variável do tipo ponteiro para inteiro, chamada p, e o conteúdo inicial é lixo;
+    p = &a; // faz p receber o endereço de a. Dizemos que p aponta para a
+    printf("%p \n", &a);
+    printf("%p", p);
+return 0;
+}
