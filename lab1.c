8d859629b9835a4545d223c3a35d82acc03adaee
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-10-04 20:11:37 -0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 
+// Define a struct para armazanenar o numero Racional
+
 struct racional{
     int numerador;
     int denominador;
@@ -20,7 +22,7 @@ Racional cria(int num, int den){
     return r;
 }
 
-
+// Funcao para multiplicar os numeros racionais
 Racional multiplica(Racional r1, Racional r2){
     Racional m;
     m.numerador = r1.numerador*r2.numerador;
@@ -32,10 +34,12 @@ Racional multiplica(Racional r1, Racional r2){
 int main()
 {
     Racional r1, r2, r3;
+    int num, den;
+
     printf("r1.n r1.d r2.n r2.d \n");
     scanf("%d %d %d %d", &r1.numerador, &r1.denominador, &r2.numerador, &r2.denominador);
 
-    r3 = multiplica(r1, r2);
+    r3 = multiplica(r1, r2);  // Chama a funcao
     printf("O resultado eh: %d/%d \n", r3.numerador, r3.denominador);
 
     return 0;
