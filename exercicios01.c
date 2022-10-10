2e54f0e58de89c17930470e56d90cb6660ea96eb
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-10-10 20:17:54 -0300

    Update exercicios01.c

--- exercicios01.c
+++ exercicios01.c
@@ -12,13 +12,13 @@ typedef struct ponto Coordenadas; // Cria o tipo de struct como Coordenadas para
 
 
 Coordenadas pontoP(int x, int y){  // Cria o ponto P com as coordenadas de entrada.
-    Coordenadas coord1;
-    coord1.x = x;
-    coord1.y = y;
-    return coord1;
+    Coordenadas coord;
+    coord.x = x;
+    coord.y = y;
+    return coord;
 }
 
-int Distancia(Coordenadas coord1, Coordenadas coord2){
+float Distancia(Coordenadas coord1, Coordenadas coord2){
     float distancia = 0;
     distancia = sqrt((coord2.x - coord1.x)+(coord2.y - coord1.y));
     return distancia;
@@ -27,11 +27,16 @@ int main() {
     int x, y;
     Coordenadas coord1;
     Coordenadas coord2;
+    float distancia1e2;
     printf("Digite as coordenadas de X e Y separadas por espaco: ");
     scanf("%d %d", &x, &y);
     coord1 = pontoP(x, y);
-    print("X = %d Y = %d", coord1.x, coord1.y);
-    
+    print("Ponto 1: X = %d Y = %d", coord1.x, coord1.y);
+    printf("Digite as coordenadas do segundo ponto separadas por espaco: ");
+    scanf("%d %d", &x, &y);
+    coord2 = pontoP(x, y);
+    distancia1e2 = Distancia(coord1, coord2);
+    print("Distancia entre os dois pontos: %f", distancia1e2);
     
     
     return 0;
