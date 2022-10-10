d135b720be16b75883c3c9e9a399f25c8780fb58
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-10-10 20:00:41 -0300

    Update exercicios01.c

--- exercicios01.c
+++ exercicios01.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ponto{
+struct ponto{  // Declara struct para armazenar as coordenadas do ponto.
   int x;
   int y;
 };
 
-typedef struct ponto Coordenadas;
+typedef struct ponto Coordenadas; // Cria o tipo de struct como Coordenadas para facilitar a leitura e escrita.
 
 
-Coordenadas pontoP(int x, int y){
+Coordenadas pontoP(int x, int y){  // Cria o ponto P com as coordenadas de entrada.
     Coordenadas coord1;
     coord1.x = x;
     coord1.y = y;
     return coord1;
 }
 
-
+int Distancia(Coordenadas coord1, Coordenadas coord2){
+    
+    
+    
+    
+}
 int main() {
     int x, y;
     Coordenadas coord1;
@@ -25,6 +30,7 @@ int main() {
     printf("Digite as coordenadas de X e Y separadas por espaco: ");
     scanf("%d %d", &x, &y);
     coord1 = pontoP(x, y);
+    print("%d %d", coord1.x, coord1.y);
     
     
     
