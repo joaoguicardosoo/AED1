f778cf31e18d2be2ef42ab649c1a3e11e82eba08
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-10-10 19:46:49 -0300

    Create exercicios01.c

---
+++ exercicios01.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ponto{
+  int x;
+  int y;
+};
+
+typedef struct ponto Coordenadas;
+
+
+int pontoP(int x, int y){
+    Coordenadas.x = x;
+    Coordenadas.y = y;
+    return 0;
+    
+    
+}
+
+
+int main() {
+    int x, y;
+    printf("Digite as coordenadas de X e Y separadas por espaco: ");
+    scanf("%d %d", &x, &y);
+    
+    
+    
+    
+    return 0;
+}
