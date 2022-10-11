be12b18f640230ac6268fe0d36b2469c13e85166
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-10-11 19:26:47 -0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,37 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
+#define  MAX  10    // tamanho maximo do vetor
 
+// Declaracao da estrutura de dados
+struct vetor {
+    int elem[MAX];  // armazena os dados do vetor
+    int tam;        // armazena o tamanho do vetor
+};
 
-// Exercicio 1
-/* a)
-void main()
+// Definicao do tipo Vetor a partir da estrutura de dados
+typedef struct vetor Vetor;
+
+/*
+ * Retorna o maior elemento do vetor
+ */
+int maior_v1(Vetor vet, int *erro)
 {
-    int a = 40; // cria uma variável do tipo inteiro, chamada a, e inicializa com valor 40;
-    int *p; // cria uma variável do tipo ponteiro para inteiro, chamada p, e o conteúdo inicial é lixo;
-    p = &a; // faz p receber o endereço de a. Dizemos que p aponta para a
-    printf("%p \n", &a);
-    printf("%p", p);
+    int i, m = -1;
+
+    // tratamento de erro
+    /* opções:
+        2 - usar parametro auxiliar para erro
+    */
+
+    if (vet.tam == 0) *erro = 1;
+    else {
+        m = vet.elem[0];
+
+        for (i = 1; i < vet.tam; i++)
+            if (vet.elem[i] > m) m = vet.elem[i];
+    }
+
+    return m;
+}
+
+int maior(Vetor vet, int *m)
+{
+    int i;
+
+    // tratamento de erro
+    /* opções:
+        1 - retornar um codigo de erro
+            (0 se funcionou, 1 caso contrario)
+    */
+
+    if (vet.tam == 0) return 1;
+
+    *m = vet.elem[0];
+    for (i = 1; i < vet.tam; i++)
+   	 if (vet.elem[i] > *m) *m = vet.elem[i];
+
     return 0;
 }
-*/
-// b)
-int main()
-{
-    int a; // cria uma variável do tipo inteiro, chamada a, e inicializa com valor 40;
-    int *p; // cria uma variável do tipo ponteiro para inteiro, chamada p, e o conteúdo inicial é lixo
-    p = &a; // faz p receber o endereço de a. Dizemos que p aponta para a
 
-    scanf ("%d", a);
-    printf("%p \n", &a);
-    printf("%p", p);
+int criar(Vetor *vet)
+{
+    vet->tam = 0;
+
+    return 0; // nao tem erro
+}
+
+// Insere o valor x no vetor vet
+int inserir(Vetor *vet, int x)
+{
+    int i;
+
+    if (vet->tam >= MAX) return 1; // erro
+    i = vet->tam;
+    vet->elem[i] = x;
+    vet->tam++;
+    return 0; // nao tem erro
+
+
+    // alternativamente
+    // vet->elem[vet->tam++] = x;
+}
+
+
+int main (){
+    Vetor v1;
+    int x;
+    printf("Digite o um valor para inserir no vetor: ");
+    scanf("%d", &x);
+    int inserir(v1, x);
+
+
+
+
+
 return 0;
 }
-/* Exercicio 2
-void main(){
-    char nome[] = "José Augusto";
-    char *p;
-    p = &nome;
-    printf("%c", p);
-}
-*/
